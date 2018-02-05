b9863bab2d680bab2ea23c25619b1834b4502bcb
Dalton Carr <daltonlcarr@yahoo.com>
2018-02-04 17:13:34 -0800

    dashboard up, with button linking to it fron landing

---
+++ src/components/landing/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Dashboard from  '../dashboard';
+
+class Landing extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      showComponent:false,
+    };
+    this._onButtonClick = this._onButtonClick.bind(this);
+  }
+
+  _onButtonClick() {
+    this.setState({
+      showComponent: true,
+    });
+  }
+
+
+  render() {
+    return (
+      <div className='landing'>
+        <BrowserRouter>
+          <div>
+              
+            <button onClick={this._onButtonClick}>Dash</button>
+            {this.state.showComponent ?
+              <Dashboard /> :
+              null
+            }
+            <button onClick={() => {
+              return <Link to="/dashboard" component={Dashboard}/>;
+            }}></button>
+          </div>
+
+          
+          
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+export default Landing;
