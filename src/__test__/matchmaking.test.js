bd12e9e6b5cca2754e7eb9c975e5b65115a75483
Catherine Looper <loopercat@gmail.com>
2018-02-05 15:49:00 -0800

    Added a matchmaking test file

---
+++ src/__test__/matchmaking.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, { shallow, render, mount } from 'enzyme';
+
+
+Enzyme.configure({adapter: new Adapter()});
+
+import Matchmaking from '../components/matchmaking';
+
+describe('Matchmaking', () => {
+  test('Testing that the initial matchmaking state sets searching : true', () => {
+    let mountedMatchmakingForm = Enzyme.mount(<Matchmaking/>);
+
+    expect(mountedMatchmakingForm.state()).toEqual({searching:true});
+  });
+});
--- src/__test__/signup-form.test.js
+++ src/__test__/signup-form.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Enzyme from 'enzyme';
+import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({adapter: new Adapter()});
@@ -12,4 +12,60 @@ describe('SignupForm', () => {
 
     expect(mountedSignUpForm.state()).toEqual({});
   });
+  test('Testing that the component renders a SignUp form to the page', () => {
+    const wrapper = shallow(
+      <form></form>
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test('Testing handleChange events', () => {
+
+    let wrapper = Enzyme.mount(<SignupForm />);
+
+    // wrapper.find('input').simulate('change');
+
+    let name = 'name';
+    let value = 'value';
+
+    // let {name, value} = event.target;
+
+    wrapper.setState({[name] : value});
+
+    expect(wrapper.state()).toEqual({[name] : value});
+  });
+
+
+  // test('Testing handleSubmit events', () => {
+  //   let wrapper = Enzyme.mount(<SignupForm />);
+  
+  //   wrapper.find('button').simulate('click');
+
+  //   let name = 'yourName';
+  //   let value = 'yourName';
+
+  //   // let {name, value} = event.target;
+    
+  //   wrapper.setProps({[name] : value});
+    
+  //   expect(wrapper.props()).toEqual({[name] : value});
+  // });
+
+  // test('pass a selected value to the onChange handler', () => {
+  //   const value = '2';
+  //   const handleChange = handleChange(event);
+  //   const wrapper = shallow(
+  //     <input onChange={handleChange} />
+  //   );
+
+  //   expect(wrapper).toMatchSnapshot();
+
+  //   wrapper.find('select').simulate('change', {
+  //     target: { value },
+  //   });
+
+  //   expect(handleChange).toBeCalledWith(value);
+  // });
+    
+
 });
