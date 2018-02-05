98262cbc8aa5f80345bd373e8509e15b1d13ae7d
Dalton Carr <daltonlcarr@yahoo.com>
2018-02-05 12:37:53 -0800

    matchmaking page and countdown

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -4,6 +4,7 @@ import GameJoin from '../join-game';
 import Home from  '../home';
 import Landing from '../landing';
 import Matchmaking from '../matchmaking';
+import Countdown from '../countdown';
 
 class App extends React.Component {
   constructor(props){
@@ -19,8 +20,7 @@ class App extends React.Component {
             <Route exact path='/' component={Home}/>
             <Route exact path='/landing' component={Landing}/>
             <Route exact path='/matchmaking' component={Matchmaking}/>
-            {/* <Route exact path ='/dashboard' component={Dashboard}/>
-            <Route exact path='/profile' component={Profile}/> */}
+            <Route exact path='/countdown' component={Countdown}/>
           </div>
         </BrowserRouter>
       </div>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
-import './style/dashboard.scss';
+import './style/main.scss';
 const container = document.createElement('div');
 document.body.appendChild(container);
 ReactDOM.render(<App />, container);
