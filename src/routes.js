43bed9db3a6520e673fad7268a836c3c6b5eea05
jjkusowski <jjkusowski@yahoo.com>
2018-02-09 08:52:02 -0800

    changed to https

--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,5 @@ export const SIGNUP_ROUTE = '/signup';
 export const HOME_ROUTE = '/';
 export const LANDING_ROUTE = '/landing';
 export const PROFILE_ROUTE = '/profiles';
-export const API_ROUTE = 'http://geo-jumper-backend.herokuapp.com';
+export const API_ROUTE = 'https://geo-jumper-backend.herokuapp.com';
 export const HIGHSCORES = '/highscores';
