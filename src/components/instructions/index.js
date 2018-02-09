7b51e212f4c46d482422bda2f50cfe74585c5be2
Catherine Looper <loopercat@gmail.com>
2018-02-08 21:25:07 -0800

    Styled results

--- src/components/instructions/index.js
+++ src/components/instructions/index.js
@@ -1,30 +1,20 @@
+import './_instructions.scss';
 import React from 'react';
 import FindGame from '../find-game';
 
-class Landing extends React.Component {
+class Instructions extends React.Component {
   constructor(props){
     super(props);
   }
   render(){
     return (
       <div id ="instructions">
-        <h1>Instructions</h1>
+        <h1>How To Play:</h1>
         <p> Move using the arrow keys and space-bar on your keyboard!</p>
-        <div id ='keys'>
-          <div id ='up'>Jump</div>
-          <div id ='left'>Move left  </div>
-          <div id ='down'>Glide</div>
-          <div id ='right'>Move right</div>
-          <div id ='space'>Jump</div>
-        </div>
-
-        {/* <li>Up arrow - Jump</li>
-        <li>Left arrow - Move left</li>
-        <li>Right arrow - Move right</li>
-        <li>Down arrow - Glide</li> */}
       </div>
     );
   }
 }
 
-export default Landing;
+export default Instructions;
+
