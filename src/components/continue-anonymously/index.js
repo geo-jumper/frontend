b980cae6756f64ee19cd6b75540712ccd379ce9c
Catherine Looper <loopercat@gmail.com>
2018-02-08 19:59:04 -0800

    Styled login signup quick play

--- src/components/continue-anonymously/index.js
+++ src/components/continue-anonymously/index.js
@@ -1,8 +1,9 @@
+import './_continue-anonymously.scss';
 import React from 'react';
 
 const ContinueAnonymously = ({ history, toggleLandingOK }) => {
   return(
-    <button onClick = {() =>{
+    <button id='quick-play' onClick = {() =>{
       sessionStorage.removeItem('X-GEO-JUMPER-TOKEN');
       toggleLandingOK();
       history.push('/landing');
